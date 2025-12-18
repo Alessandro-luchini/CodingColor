72d772d78a64fcfa8733ff9a0836297f81d77a32
Alessandro-luchini <alessandroluchini2008@gmail.com>
2025-12-18 10:42:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ console.log("Inizio esecuzione");
 let d = new date();
 console.log(d);
 document.getElementById("saluto").innerHTML = d.getDay();
+let colori = [];
+let colori2 = new Array();
+
 function elabora() {
     let numero = document.getElementById("inNumber").value;
     alert(numero);
     
     if (numero >=0 && numero <=255){
+        colori.push(numero);
         document.getElementById("outText").innerHTML = "Hai inserito il numero corretto";
+        document.getElementById("outText").innerHTML += "<br> i colori inseriti sono: " + colori;
+        console.log(colori);
     }
     else{
         document.getElementById("error").innerHTML = "Il numero non è corretto"
